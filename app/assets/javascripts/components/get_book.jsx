9f2adeffd6d382755937fa6e51920a1b9f96014d
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2018-02-14 20:46:14 +0800

    first commit crud

---
+++ app/assets/javascripts/components/get_book.jsx
@@ -0,0 +1,7 @@
+window.GetBook = React.createClass({
+    render: function() {
+      return (
+        <h1>Hello</h1>
+      )
+    }
+  });
---
+++ app/controllers/mahasiswa_controller.rb
@@ -0,0 +1,2 @@
+class MahasiswaController < ApplicationController
+end
